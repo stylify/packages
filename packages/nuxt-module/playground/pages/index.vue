011617ea0425b22c2e6dff7f40ce9f4cce51a48e
Vladimir Machacek <8machy@seznam.cz>
2021-09-19 17:58:36 +0200

    Use only one file for cache

--- packages/nuxt-module/playground/pages/index.vue
+++ packages/nuxt-module/playground/pages/index.vue
@@ -4,8 +4,6 @@
 		<div>
 			<NuxtLink to="/second">Go to second page</NuxtLink>
 		</div>
-		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2572.969991046954!2d15.99401371609492!3d49.84302137939606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470dbec925c65f2d%3A0xad682f88701ca238!2zU2t1dGXEjSwgbsOhbS4!5e0!3m2!1scs!2scz!4v1631651072861!5m2!1scs!2scz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-		<iframe style="border:none" src="https://frame.mapy.cz/s/fogetomaja" width="400" height="280" frameborder="0"></iframe>
 	</div>
 </template>
 
