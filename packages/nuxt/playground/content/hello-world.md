72b46e2396167bd29d80fb22f7085eea83ff4214
Vladimir Machacek <8machy@seznam.cz>
2022-06-04 15:28:54 +0000

    Add content into the playground for testing nuxt content

---
+++ packages/nuxt/playground/content/hello-world.md
@@ -0,0 +1,1 @@
+<div class="color:green font-weight:bold">Nuxt module playground!</div>
--- packages/nuxt/playground/nuxt.config.ts
+++ packages/nuxt/playground/nuxt.config.ts
@@ -1,7 +1,11 @@
 import { defineNuxtConfig } from 'nuxt';
 
 export default defineNuxtConfig({
+	server: {
+		host: '0.0.0.0'
+	},
 	modules: [
+		['@nuxt/content'],
 		['../esm/module.js', { addPlugin: true }]
 	]
 });
