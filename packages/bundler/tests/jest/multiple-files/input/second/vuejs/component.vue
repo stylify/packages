ca02c282230c2b9cc8790a398d3fb9491a1945e1
Vladimir Machacek <8machy@seznam.cz>
2022-07-02 23:00:49 +0200

    Content options tunning

--- packages/bundler/tests/jest/multiple-files/input/second/vuejs/component.vue
+++ packages/bundler/tests/jest/multiple-files/input/second/vuejs/component.vue
@@ -1,4 +1,4 @@
-<!-- @stylify-pregenerate content:^vuejs__component^ /@stylify-pregenerate -->
+<!-- stylify-pregenerate content:^vuejs__component^ /stylify-pregenerate -->
 
 <template>
 	<div v-bind:class="{ 'font-size:48px': true }"></div>
