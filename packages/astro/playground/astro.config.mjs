f80fa66a0c239eda5c8bca4799fcc3cda8c0f1ca
Vladimír <8machy@seznam.cz>
2022-12-12 16:35:22 +0000

    Astro playground tunning

--- packages/astro/playground/astro.config.mjs
+++ packages/astro/playground/astro.config.mjs
@@ -7,28 +7,39 @@ import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 
+const pagesDir = 'src/pages';
+const layoutsDir = 'src/layouts';
+
 /** @type { import('@stylify/bundler').BundlerConfigInterface[]} */
 const stylifyBundles = [];
 const layoutCssLayerName = 'layout';
 const pageCssLayerName = 'page';
 
-const getFileCssLayerName = (filePath) => filePath.includes('/pages/') ? pageCssLayerName : layoutCssLayerName;
+const getFileCssLayerName = (filePath) =>
+  filePath.includes('/pages/') ? pageCssLayerName : layoutCssLayerName;
+
+const getOutputFileName = (file) => {
+  const parsedFile = path.parse(file);
+  const fileName = parsedFile.name.toLowerCase();
+  const dirNameCleanupRegExp = new RegExp(`${pagesDir}|${layoutsDir}|\\W`, 'g');
+  const dir = parsedFile.dir.replace(dirNameCleanupRegExp, '');
+  return `${dir.length ? `${dir}-` : ''}${fileName}.css`;
+};
 
 const createBundle = (file) => {
-	const fileName = path.parse(file).name;
-	const fileCssLayerName = getFileCssLayerName(file);
+  const fileCssLayerName = getFileCssLayerName(file);
 
-	return {
-		outputFile: `src/styles/${fileCssLayerName}/${fileName.toLowerCase()}.css`,
-		files: [file],
-		cssLayer: fileCssLayerName
-	};
+  return {
+    outputFile: `src/styles/${fileCssLayerName}/${getOutputFileName(file)}`,
+    files: [file],
+    cssLayer: fileCssLayerName,
+  };
 };
 
 const createBundles = (files) => {
-	for (const file of files) {
-		stylifyBundles.push(createBundle(file));
-	}
+  for (const file of files) {
+    stylifyBundles.push(createBundle(file));
+  }
 };
 
 // 1. Map files in layouts/pages and create bundles
@@ -37,74 +48,82 @@ createBundles(fastGlob.sync('src/layouts/**/*.astro'));
 
 // 2. Init Stylify Astro Integraton
 const stylifyIntegration = stylify({
-	bundler: {
-		id: 'astro',
-		// Set CSS @layers order
-		cssLayersOrder: {
-			// Order will be @layer layout,page;
-			order: [layoutCssLayerName, pageCssLayerName].join(','),
-			// Layers order will be exported into file with layout @layer
-			exportLayer: [layoutCssLayerName]
-		}
-	},
-	bundles: stylifyBundles
+  bundler: {
+    id: 'astro',
+    // Set CSS @layers order
+    cssLayersOrder: {
+      // Order will be @layer layout,page;
+      order: [layoutCssLayerName, pageCssLayerName].join(','),
+      // Layers order will be exported into file with layout @layer
+      exportLayer: [layoutCssLayerName],
+    },
+  },
+  bundles: stylifyBundles,
 });
 
 // 3. Add hook that processes opened files
 /** @param { import('@stylify/bundler').BundleFileDataInterface } data */
 hooks.addListener('bundler:fileToProcessOpened', (data) => {
-	let { content, filePath } = data;
+  let { content, filePath } = data;
 
-	// 3.1 Only for root files
-	if (data.isRoot) {
-		const cssFileName = path.parse(filePath).name;
-		const cssFilePathImport = `import '/src/styles/${getFileCssLayerName(filePath)}/${cssFileName.toLowerCase()}.css';`;
+  // 3.1 Only for root files
+  if (data.isRoot) {
+    const cssFileName = path.parse(filePath).name;
+    const cssFilePathImport = `import '/src/styles/${getFileCssLayerName(
+      filePath
+    )}/${getOutputFileName(filePath)}';`;
 
-		if (!content.includes(cssFilePathImport)) {
-			if ((/import \S+ from (?:'|")\S+(\/layouts\/\S+)(?:'|");/).test(content)) {
-				content = content.replace(
-					/import \S+ from (?:'|")\S+\/layouts\/\S+(?:'|");/, `$&\n${cssFilePathImport}`
-				);
-			} else if ((/^\s*---\n/).test(content)) {
-				content = content.replace(/^(\s*)---\n/, `$&${cssFilePathImport}\n`);
-			} else {
-				content = `---\n${cssFilePathImport}\n---\n${content}`;
-			}
+    if (!content.includes(cssFilePathImport)) {
+      if (/import \S+ from (?:'|")\S+(\/layouts\/\S+)(?:'|");/.test(content)) {
+        content = content.replace(
+          /import \S+ from (?:'|")\S+\/layouts\/\S+(?:'|");/,
+          `$&\n${cssFilePathImport}`
+        );
+      } else if (/^\s*---\n/.test(content)) {
+        content = content.replace(/^(\s*)---\n/, `$&${cssFilePathImport}\n`);
+      } else {
+        content = `---\n${cssFilePathImport}\n---\n${content}`;
+      }
 
-			fs.writeFileSync(filePath, content);
-		}
-	}
+      fs.writeFileSync(filePath, content);
+    }
+  }
 
-	// 3.2 For all files
-	const regExp = new RegExp(`import \\S+ from (?:'|")\\S+(\\/components\\/\\S+)(?:'|");`, 'g');
-	let importedComponent;
-	const importedComponentFiles = [];
-	const rootDir = path.dirname(fileURLToPath(import.meta.url));
+  // 3.2 For all files
+  const regExp = new RegExp(
+    `import \\S+ from (?:'|")\\S+(\\/components\\/\\S+)(?:'|");`,
+    'g'
+  );
+  let importedComponent;
+  const importedComponentFiles = [];
+  const rootDir = path.dirname(fileURLToPath(import.meta.url));
 
-	while (importedComponent = regExp.exec(content)) {
-		importedComponentFiles.push(path.join(rootDir, 'src', importedComponent[1]));
-	}
+  while ((importedComponent = regExp.exec(content))) {
+    importedComponentFiles.push(
+      path.join(rootDir, 'src', importedComponent[1])
+    );
+  }
 
-	data.contentOptions.files = importedComponentFiles;
+  data.contentOptions.files = importedComponentFiles;
 });
 
 // 4. Wait for bundler to initialize and watch for directories
 // to create new bundles when a file is added
 hooks.addListener('bundler:initialized', ({ bundler }) => {
-	// Watch layouts and pages directories
-	const dirsToWatchForNewBundles = ['layouts', 'pages'];
-	for (const dir of dirsToWatchForNewBundles) {
-		const pathToWatch = `src/${dir}`;
-		fs.watch(pathToWatch, (eventType, fileName) => {
-			const fileFullPath = path.join(pathToWatch, fileName);
+  // Watch layouts and pages directories
+  const dirsToWatchForNewBundles = ['layouts', 'pages'];
+  for (const dir of dirsToWatchForNewBundles) {
+    const pathToWatch = `src/${dir}`;
+    fs.watch(pathToWatch, (eventType, fileName) => {
+      const fileFullPath = path.join(pathToWatch, fileName);
 
-			if (eventType !== 'rename' || !fs.existsSync(fileFullPath)) {
-				return;
-			}
+      if (eventType !== 'rename' || !fs.existsSync(fileFullPath)) {
+        return;
+      }
 
-			bundler.bundle([createBundle(fileFullPath)]);
-		});
-	}
+      bundler.bundle([createBundle(fileFullPath)]);
+    });
+  }
 });
 
 export default defineConfig({
