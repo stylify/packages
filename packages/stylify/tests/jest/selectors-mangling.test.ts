676d05828d51c39eaf80150561bea303b6309127
Vladimír <8machy@seznam.cz>
2023-01-10 18:16:33 +0000

    Unite selector mangling tests

---
+++ packages/stylify/tests/jest/selectors-mangling.test.ts
@@ -0,0 +1,76 @@
+import TestUtils from '../../../../tests/TestUtils';
+import { Compiler } from '../../src';
+
+const testName = 'selectors-mangling';
+const testUtils = new TestUtils('stylify', testName);
+
+/**
+ * This test is here, because dollars were removed when content was mangled. It is becuase $$ is replaced by $
+ * and it broke some of the functionality in some codes when mangled.
+ */
+/* test('Mangle with special characters - dollars should stay as they are and non should removed.', (): void => {
+	const fileName = 'mangle-with-special-characters';
+ 	const inputContent = testUtils.getHtmlInputFile(fileName);
+
+	const compiler = new Compiler({
+		dev: true,
+		mangleSelectors: true
+	});
+
+	let compilationResult = compiler.compile(inputContent);
+	testUtils.testCssFileToBe(compilationResult.generateCss(), fileName);
+	testUtils.testHtmlFileToBe(compiler.rewriteSelectors(inputContent), fileName);
+});
+
+test('Mangle single letter macros', (): void => {
+	const fileName = 'mangle-single-letters-macros';
+	const inputContent = testUtils.getHtmlInputFile(fileName);
+
+	const compiler = new Compiler({
+		dev: true,
+		mangleSelectors: true,
+		macros: {
+			'm:(\\S+?)': ({macroMatch, selectorProperties}) => {
+				selectorProperties.add('margin', macroMatch.getCapture(0));
+			}
+		}
+	});
+	let compilationResult = compiler.compile(inputContent);
+
+	testUtils.testCssFileToBe(compilationResult.generateCss(), fileName);
+	testUtils.testHtmlFileToBe(compiler.rewriteSelectors(inputContent), fileName);
+});
+
+test('Duplicate selectors', (): void => {
+	const fileName = 'mangle-single-letters-macros';
+	const inputContent = testUtils.getHtmlInputFile(fileName);
+
+	const compiler = new Compiler({ mangleSelectors: true, dev:true });
+	let compilationResult = compiler.compile(inputContent);
+
+	testUtils.testCssFileToBe(compilationResult.generateCss(), fileName);
+	testUtils.testHtmlFileToBe(compiler.rewriteSelectors(inputContent), fileName);
+});
+ */
+test('Component selector similar to custom selector', (): void => {
+	const fileName = 'components-and-custom-selectors-collision';
+	const inputContent = testUtils.getHtmlInputFile(fileName);
+
+	const compiler = new Compiler({
+		dev: true,
+		mangleSelectors: true,
+		ignoredAreas: [
+			/(-link)/
+		],
+		customSelectors: {
+			'.docs__section-link': 'font-weight:bold'
+		},
+		components: {
+			link: 'color:blue'
+		}
+	});
+	let compilationResult = compiler.compile(inputContent);
+
+	testUtils.testCssFileToBe(compilationResult.generateCss(), fileName);
+	testUtils.testHtmlFileToBe(compiler.rewriteSelectors(inputContent), fileName);
+});
