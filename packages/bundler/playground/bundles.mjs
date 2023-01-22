c7aa36162936e6227f964e65c4e293e253e36306
Vladimír <8machy@seznam.cz>
2023-01-22 22:57:55 +0000

    Bundler playground tunning

--- packages/bundler/playground/bundles.mjs
+++ packages/bundler/playground/bundles.mjs
@@ -1,11 +1,16 @@
 import path from 'path';
 import { Bundler } from '../esm/index.mjs';
 
+const watchFiles = process.argv[process.argv.length - 1] === '--w';
+
 const outputDir = path.join('.', path.sep, 'css');
 const bundler = new Bundler({
-	dev: true,
-	verbose: true,
-	watchFiles: true,
+	dev: watchFiles,
+	verbose: watchFiles,
+	watchFiles: watchFiles,
+	compiler: {
+		mangleSelectors: !watchFiles
+	},
 	bundles: [
 		{
 			outputFile: path.join(outputDir, 'index.css'),
