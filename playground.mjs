c189f4675994045a4007a89dd7d334e488d94db1
Vladimír <8machy@seznam.cz>
2023-02-05 20:28:18 +0000

    Add playground for node

---
+++ playground.mjs
@@ -0,0 +1,21 @@
+import { Compiler } from './packages/stylify/esm/index.mjs';
+
+const compiler = new Compiler({
+	dev: true,
+	mangleSelectors: false,
+	variables: {
+
+	},
+	components: {
+
+	}
+});
+
+const content = `
+
+`.trim();
+
+const compilationResult = compiler.compile(content);
+console.log('\n\n-------------');
+console.log(compiler.rewriteSelectors(content));
+console.log(compilationResult.generateCss());
